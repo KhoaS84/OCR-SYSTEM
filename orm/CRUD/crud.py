fc7ab44e38a7c202a8af7917a2c8fecbc71cfe7a
XuanVinhNgo <157783476+XuanVinhNgo@users.noreply.github.com>
2026-01-16 14:50:49 +0700

    Delete orm/CRUD/crud.py

--- orm/CRUD/crud.py
+++
@@ -1,7 +0,0 @@
-from models.user import User
-from models.citizens import Citizens
-from models.documents import Documents, CCCD, BHYT, DocumentImages
-from models.ocr import ocr_jobs, ocr_results
-from django.utils import timezone
-from django.db import connection
-from pprint import pprint
