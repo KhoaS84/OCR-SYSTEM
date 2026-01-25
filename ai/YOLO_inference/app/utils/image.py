942d1fcdc90b5d4bac20f2341dac793f1fd02bd1
Im-Shocker <giacatdu1412@gmail.com>
2026-01-25 16:58:27 +0700

    chore(dev): remove old ai folder to re-add as subtree

--- ai/YOLO_inference/app/utils/image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-def read_image(file_bytes: bytes) -> np.ndarray:
-    img_np = np.frombuffer(file_bytes, np.uint8)
-    image = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-    if image is None:
-        raise ValueError("Invalid image file")
-    return image
