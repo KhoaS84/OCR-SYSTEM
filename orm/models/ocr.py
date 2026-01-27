920e0ab65053aee24cfa00467a7507b634fb5164
XuanVinhNgo <157783476+XuanVinhNgo@users.noreply.github.com>
2026-01-27 16:19:05 +0700

    Update ocr.py

--- orm/models/ocr.py
+++ orm/models/ocr.py
@@ -49,8 +49,11 @@ class ocr_jobs (models.Model):
 
 class ocr_results(models.Model):
 
-    id = models.AutoField(
-        primary_key=True
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
     )
     ocr_job = models.ForeignKey(
         ocr_jobs,
@@ -67,4 +70,5 @@ class ocr_results(models.Model):
 
     def __str__(self):
         return f"{self.field_name}: {self.raw_text}"
+
 
